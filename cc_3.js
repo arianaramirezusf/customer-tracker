f56937363b2fe757ecb25a3bdd8aca04cefc1a1c
arianaramirezusf <arianaramirez@usf.edu>
2026-02-08 13:53:59 -0500

    Creating Customer Array

---
+++ cc_3.js
@@ -0,0 +1,19 @@
+let customers = [
+    {
+        name: "Jason",
+        email:"jason.ramirez@gmail.com",
+        purchases: ["shoes", "pants", "socks"]
+    },
+{ 
+    name: "Carmen",
+    email: "carmen.johnson@gmail.com",
+    purchases: ["dress", "shoes", "hat"]
+},
+
+{
+    name: "Jaylene",
+    email: "jaylene.wilson@gmail.com",
+    purchases: ["jacket", "jeans", "sweater"]
+}
+]
+
