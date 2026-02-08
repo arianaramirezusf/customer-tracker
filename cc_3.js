feae4c0c4fee18473ab6200d9a2fd9508dd62f30
arianaramirezusf <arianaramirez@usf.edu>
2026-02-08 14:14:33 -0500

    Looping Customer Array

--- cc_3.js
+++ cc_3.js
@@ -30,3 +30,12 @@ customers.shift({
 })
 
 console.log(customers);
+customers[1].purchases.push("scarf");
+customers[3].email= "alex.ronald123@gmail.com"
+
+customers.forEach(customer => {
+    console.log(customer.name);
+    console.log(customer.email);
+    console.log(customer.purchases);
+})
+
